8e852a907ef0ce53657885f1c4b301676c4f1d16
hu <hu@qq.com>
2019-10-18 19:43:33 +0800

    test setup

---
+++ app/js/core/slider.vue
@@ -0,0 +1,49 @@
+<!--  -->
+<template lang="html">
+  <section :class="cname">
+    <swiper :options="options" :not-next-click="notNextClick">
+      <swiperSlide v-for="item in items" :key="item.href">
+        <router-link :to="{name : item.href}">
+          <img :src="item.src" alt="">
+        </router-link>
+      </swiperSlide>
+      <div class="swiper-pagination" v-if="options.pagination" slot="pagination"/>
+    </swiper>
+  </section>
+</template>
+
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  components: { swiper, swiperSlide },
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          autoplay: true,
+          loop: true,
+          notNextClick: false
+        }
+      }
+    },
+    cname: {
+      type: String,
+      default: ''
+    },
+    items: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+
+</script>
+<style lang='scss'  >
+@import "~swiper/dist/css/swiper.css";
+</style>
---
+++ app/js/public/header.vue
@@ -0,0 +1,49 @@
+<!--  -->
+<template lang="html">
+  <div :class="$style.header">
+    <span :class="$style.left">
+      <em>注册</em>&nbsp;|&nbsp;<em>登录</em>
+    </span>
+    <btn :class="$style.btnDownload">APP下载</btn>
+  </div>
+
+</template>
+
+<script>
+import btn from '../core/btn.vue'
+export default {
+  components: { btn }
+}
+
+</script>
+<style lang='scss' module>
+.header {
+  color: #666;
+  height: 100px;
+  line-height: 100px;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  font-size: 32px;
+  background: #fff url("../../images/jr-logo.png") center center no-repeat;
+  background-size: auto 50%;
+  z-index: 100px;
+  .left {
+    font-size: 28px;
+    height: 30px;
+    line-height: 30px;
+    margin: 17px 0 0 18px;
+  }
+  .btnDownload {
+    float: right;
+    font-size: 24px !important;
+    border-width: 0;
+    height: 56px;
+    line-height: 56px !important;
+    min-width: 120px;
+    border-radius: 4px;
+    margin: 28px 24px 0 0;
+  }
+}
+</style>
